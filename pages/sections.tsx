aa558cb41d7402bc5d81c0a9e1f713665775dc5f
francomd <francomdalmasso@gmail.com>
2021-03-02 22:31:10 -0300

    WIP: JSON to fields

--- pages/sections.tsx
+++ pages/sections.tsx
@@ -1,6 +1,76 @@
+import { Card, Collapse, Input, Select } from "antd";
+import Title from "antd/lib/typography/Title";
 import Head from "next/head";
 
-export default function Sections() {
+const INPUTS_ELEM = {
+  input: Input,
+  select: Select,
+};
+
+const MOCK_SECTION = [
+  {
+    component_id: "1",
+    title: "Form",
+    inputs: [
+      {
+        type: "input",
+        props: {
+          label: "Name",
+          placeholder: "Insert your name",
+        },
+      },
+      {
+        type: "select",
+        props: {
+          label: "Framework",
+          placeholder: "Select your framework",
+          options: [
+            {
+              label: "Vue",
+              value: 0,
+            },
+            {
+              label: "React",
+              value: 1,
+            },
+            {
+              label: "Svelte",
+              value: 2,
+            },
+            {
+              label: "Angular... en serio?",
+              value: 3,
+            },
+          ],
+        },
+      },
+    ],
+  },
+];
+
+const SubElements = ({ props, type }) => {
+  const Comp = INPUTS_ELEM[type];
+  return (
+    <>
+      <Comp {...props} />
+    </>
+  );
+};
+
+const Elements = ({ title, inputs }) => (
+  <>
+    <Title level={4}>{title}</Title>
+    <Collapse defaultActiveKey={["0"]}>
+      {inputs.map((input, i) => (
+        <Collapse.Panel header={`Edit the ${input.props.label}`} key={i}>
+          <SubElements {...input} />
+        </Collapse.Panel>
+      ))}
+    </Collapse>
+  </>
+);
+
+export default function Sections(): JSX.Element {
   return (
     <>
       <Head>
@@ -8,7 +78,11 @@ export default function Sections() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Test</h1>
+      <Card title="Section 1">
+        {MOCK_SECTION.map((elements) => (
+          <Elements {...elements} key={elements.component_id} />
+        ))}
+      </Card>
     </>
   );
 }
